089d5220a09a91d3d2798f22ffbf325e9eb0240b
top_acdemy_student <top_acdemy_student@07-07>
2025-01-20 16:14:48 +0700

    Start

---
+++ GroupProject/Application_1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GroupProject
+{
+    /// <summary>
+    /// Логика взаимодействия для Application_1.xaml
+    /// </summary>
+    public partial class Application_1 : Window
+    {
+        public Application_1()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
