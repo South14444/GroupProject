b5ab050d58ef692c47da3cfd5a30d7a6a74cfbf2
top_acdemy_student <top_acdemy_student@07-07>
2025-01-20 17:57:10 +0700

    Второе окно

---
+++ GroupProject/Application_2.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GroupProject
+{
+    /// <summary>
+    /// Логика взаимодействия для Application_2.xaml
+    /// </summary>
+    public partial class Application_2 : Window
+    {
+        public Application_2()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
