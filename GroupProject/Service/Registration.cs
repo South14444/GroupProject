0a3bc53466c59d60b3458813ae13ce0e593c33b4
top_acdemy_student <top_acdemy_student@07-08>
2025-01-20 16:33:05 +0700

    Base

---
+++ GroupProject/Service/Registration.cs
@@ -0,0 +1,31 @@
+﻿using GroupProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static GroupProject.Models.Context;
+
+namespace GroupProject.Service
+{
+    internal class Registration
+    {
+        public async Task RegisterUserAsync(User user)
+        {
+            using (var context = new UserDbContext())
+            {
+                var existingUser = await context.Users
+                    .FirstOrDefaultAsync(u => u.Email == user.Email);
+
+                if (existingUser != null)
+                {
+                    throw new Exception("Пользователь с таким email уже существует.");
+                }
+
+                context.Users.Add(user);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
